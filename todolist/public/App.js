d792ed3f8150c189b311d2b42b7596062521d4e8
Jayson Nguyen <jaysonn@uci.edu>
2023-07-22 17:45:48 -0700

    display hidden to do list when task list not empty

--- todolist/public/App.js
+++ todolist/public/App.js
@@ -1,11 +1,44 @@
-import './App.css';
-  const notDone = []
-  const setNotDone = [];
+const newTask = document.getElementById("new-task");
+const submitButton = document.getElementById("submit");
+const recordedTasks = document.getElementById("tasks");
+// const wheelOption = document.getElementsByClassName("wheel-option");
+const wheelButton = document.getElementById("wheel-button");
 
-  function addTask() {
-    const taskInput = document.getElementById("task");
-    const newTask = taskInput.value;
-    notDone.push(newTask)
-  }
+const notDone = [];
+const done = [];
 
+function addTask() {
+    // var newTask = document.getElementById("new-task");
+    if (newTask.value === "") {
+        alert("Please add a task");
+    }
+    else
+    {
+        notDone.push(newTask.value);
+        if (recordedTasks.style.display === "") {
+            recordedTasks.style.display = "block";
+        } 
+        newTask.value = null;
+        console.log(notDone);
+    }
+    
+}
 
+function completeTask(index) {
+    done.push(notDone[index]);
+    delete notDone[index];
+}
+
+function deleteTask() {
+    var taskList = document.getElementById("task-list");
+    if (notDone.length == 0 && done.length == 0) {
+        taskList.style.display = "none";
+    } 
+    else {
+        taskList.style.display = "block";
+    }
+}
+
+function spinWheel() {
+
+}
